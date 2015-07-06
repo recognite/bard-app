9c4b5d51ebf619e848e4ab5846daa186c3f86e79
Andy Kelly <andrew.kelly@webinnovate.ie>
2015-07-06 20:51:55 +0100

    Audio not working yet on device.

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -38,7 +38,8 @@ angular.module('ionicApp', ['ionic'])
 		url: "/home",
 		views: {
 			'menuContent': {
-				templateUrl: "templates/home.html"
+				templateUrl: "templates/home.html",
+				controller: "HomeController"
 			}
 		}
     })
@@ -109,6 +110,26 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('MainController', function($scope) {
 
+})
+
+
+.controller('HomeController', function($scope) {
+	$scope.modules = [
+		{progress: "done"},
+		{progress: "done"},
+		{progress: "done"},
+		{progress: "done"},
+		{progress: "todo"},
+		{progress: "todo"},
+		{progress: "todo"},
+		{progress: "todo"},
+		{progress: "todo"},
+		{progress: "todo"}
+	];
+
+	/*
+	 * Functions for incrementing progress go here!
+	 */
 })
 
 
@@ -228,6 +249,8 @@ angular.module('ionicApp', ['ionic'])
 	$scope.$on('modal.removed', function() {
 		// Execute action
 	});
+
+	$scope.player = {title: "The Tale of the Bard", link: "http://site255.webelevate.net/bard/stories/bard-intro.mp3"};
 })
 
 
