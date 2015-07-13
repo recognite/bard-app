2b50ceae565d7bec0968ecc961111335bd9a6df9
Andy Kelly <andrew.kelly@webinnovate.ie>
2015-07-13 20:24:27 +0100

    Added a (very rudimentary) slider to story.html

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,6 +55,30 @@ angular.module('BardApp.controllers', [])
 
 .controller('StoryController', function($scope, ContentSvc) {
 
+	function showBanner(index) {
+		var oldElm = document.querySelector('.slider ion-slide.slider-slide.current');
+		var q = '.slider ion-slide.slider-slide[data-index="' + index + '"]';
+		var elm = document.querySelector(q);
+
+		console.log("Show banner " + index);
+
+		// Remove class "current"
+		if (null !== oldElm) {
+			oldElm.classList.remove("current");
+		}
+
+		// Add class "current" to current slide
+		if (null !== elm) {
+			elm.classList.add("current");
+		}
+	}
+
+	$scope.activeSlide = 0;
+	setTimeout(function() {
+		showBanner($scope.activeSlide);
+	}, 100);
+	$scope.slideChanged = showBanner;
+
 	$scope.myths = ContentSvc.StoryContent();
 })
 
